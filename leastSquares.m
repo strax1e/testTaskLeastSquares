0ea98e2b0796edf22f7a66831056841c66ffb9ae
strax1e <strax1e@yandex.ru>
2021-04-28 15:12:10 +0300

    leastSquares: comments added

--- leastSquares.m
+++ leastSquares.m
@@ -1,11 +1,11 @@
-function coefficients = leastSquares(x, y, n) % 1 param 5 degree poly is a model
-    A = [];
-    for j = 1 : length(x)
-        row = []; 
+function polyCoefficients = leastSquares(x, y, n) % можно выбрать степень n полинома
+    X = [];
+    for j = 1 : length(x) % конструируется матрица X
+        row = []; % создаем по строке
         for i = n : -1 : 0
-            row = [row  x(j).^i];
+            row = [row  x(j).^i]; % элементы в строке - это все степени какого-то одного элемента из наблюдений x (второй аргумент)
         end
-        A = [A; row];
+        X = [X; row]; % присоединяем строку к основной матрице
     end
-    coefficients = (linsolve((A' * A), (A' * y')))';
+    polyCoefficients = (linsolve((X' * X), (X' * y')))'; % решаем СЛАУ XX'b = X'y', b = polyCoefficients
 end
