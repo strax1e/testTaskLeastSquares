c9f22b421dc713c6073e7617f919d67035790583
strax1e <strax1e@yandex.ru>
2021-04-28 15:12:26 +0300

    main: comments added

--- main.m
+++ main.m
@@ -1,8 +1,8 @@
-n = 8;
+n = 8; % аргументы
 x = [1, -2, 5, 7, 9];
 y = [0, 3, 3, -4, 5];
 
-polyCoeffs = leastSquares(x, y, n)
+polyCoeffs = leastSquares(x, y, n) % получения коэф-ов полинома
 
 y1 = [];
 x1 = [];
@@ -11,8 +11,8 @@ for i = -10 : 0.1 : 10
     x1 = [x1 i];
 end
 
-plot(x1, y1)
+plot(x1, y1) % рисуем функцию найденного полинома
 hold on
-plot(x, y, 'bo')
+plot(x, y, 'bo') % рисуем изначальные точки
 grid on
 axis([-11 10 -11 10])
